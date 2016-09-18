2fefd46e11d2a487cc8e8b3379644581ab75102c
glassysky <873661157@qq.com>
2016-09-18 21:51:36 +0800

    add less loader

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,9 @@
+<style lang="less">
+@import './index.less';
+</style>
+
 <template>
   <div id="app">
-    <hello></hello>
     <p>
       Welcome to your Vue.js app!
     </p>
@@ -22,42 +25,4 @@
 </template>
 
 <script>
-import Hello from '../../components/Hello';
-
-export default {
-  components: {
-    Hello,
-  },
-};
 </script>
-
-<style>
-html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
-</style>
