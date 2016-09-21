72db2a956c312523753afd6c1d57c92ca9c903ea
glassysky <873661157@qq.com>
2016-09-22 01:13:52 +0800

    sign in

--- src/pages/signIn/index.vue
+++ src/pages/signIn/index.vue
@@ -1,8 +1,31 @@
 <style lang="less">
+@import "./index.less";
 </style>
 
 <template lang="html">
-  <h1>sign in</h1>
+  <div class="signin_background">
+    <div class="signin_container">
+      <div class="header">
+        <h1>登陆</h1>
+      </div>
+      <div class="body">
+        <div class="form">
+          <div class="form_control">
+            <label for="">学号</label>
+            <input type="text">
+          </div>
+          <div class="form_control">
+            <label for="">密码</label>
+            <input type="password">
+          </div>
+          <div class="form_control">
+            <button type="button" name="button">登陆</button>
+          </div>
+        </div>
+      </div>
+      <div class="footer"></div>
+    </div>
+  </div>
 </template>
 
 <script>
