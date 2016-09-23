6d3d68eb79c05d64663362ceea021be64847f5f5
glassysky <873661157@qq.com>
2016-09-23 23:07:47 +0800

    sign in page

--- src/pages/signIn/index.vue
+++ src/pages/signIn/index.vue
@@ -9,17 +9,27 @@
         <h1>登陆</h1>
       </div>
       <div class="body">
-        <div class="form">
-          <div class="form_control">
-            <label for="">学号</label>
-            <input type="text">
+        <div class="form_wrap">
+          <div class="form_header">
+            cast > 登陆
           </div>
-          <div class="form_control">
-            <label for="">密码</label>
-            <input type="password">
-          </div>
-          <div class="form_control">
-            <button type="button" name="button">登陆</button>
+          <div class="form">
+            <div class="form_control">
+              <label for="">学号</label>
+              <input type="text">
+            </div>
+            <div class="form_control">
+              <label for="">密码</label>
+              <input type="password">
+            </div>
+            <div class="form_control">
+              <button
+                type="button"
+                name="button"
+              >
+                登陆
+              </button>
+            </div>
           </div>
         </div>
       </div>
