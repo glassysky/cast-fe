518ced306237ab3792fa010bb68f6ed57e3b85a3
glassysky <873661157@qq.com>
2016-09-24 01:09:12 +0800

    set routers

--- src/components/navigator/index.vue
+++ src/components/navigator/index.vue
@@ -6,13 +6,23 @@
   <div class="navigator">
     <ul class="left_menu">
       <li><a href="javascript:void(0)">CAST</a></li>
-      <li class="nav_item"><a href="">首页</a></li>
-      <li class="nav_item"><a href="">活动</a></li>
-      <li class="nav_item"><a href="">报名</a></li>
+      <li class="nav_item">
+        <a v-link="{ path: '/' }">首页</a>
+      </li>
+      <li class="nav_item">
+        <a v-link="{ path: '/news' }">新闻</a>
+      </li>
+      <li class="nav_item">
+        <a v-link="{ path: '/activity' }">活动</a>
+      </li>
     </ul>
     <ul class="right_menu">
-      <li class="nav_item"><a v-link="{ path: '/sign-in' }">登陆</a></li>
-      <li class="nav_item"><a v-link="{ path: '/sign-up' }">注册</a></li>
+      <li class="nav_item">
+        <a v-link="{ path: '/sign-in' }">登陆</a>
+      </li>
+      <li class="nav_item">
+        <a v-link="{ path: '/sign-up' }">注册</a>
+      </li>
     </ul>
   </div>
 </template>
--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import VueRouter from 'vue-router';
 import Home from './pages/home/index';
 import SignIn from './pages/signIn/index';
 import SignUp from './pages/signUp/index';
+import Activity from './pages/activity/index';
+import News from './pages/news/index';
 
 Vue.use(VueRouter);
 
@@ -18,6 +20,12 @@ const router = new VueRouter();
 router.map({
   '/': {
     component: Home,
+  },
+  '/activity': {
+    component: Activity,
+  },
+  '/news': {
+    component: News,
   },
   '/sign-in': {
     component: SignIn,
