6ab6a2a88b0bac815857d70112800e21c50e260e
glassysky <873661157@qq.com>
2016-09-26 22:06:27 +0800

    change nav frame

--- src/components/navigator/index.vue
+++ src/components/navigator/index.vue
@@ -5,7 +5,13 @@
 <template>
   <div class="navigator">
     <ul class="left_menu">
-      <li><a href="javascript:void(0)">CAST</a></li>
+      <li>
+        <a
+          href="javascript:void(0)"
+          class="band_name">CAST</a>
+      </li>
+    </ul>
+    <ul class="right_menu">
       <li class="nav_item">
         <a v-link="{ path: '/' }">首页</a>
       </li>
@@ -15,13 +21,21 @@
       <li class="nav_item">
         <a v-link="{ path: '/activity' }">活动</a>
       </li>
-    </ul>
-    <ul class="right_menu">
       <li class="nav_item">
-        <a v-link="{ path: '/sign-in' }">登陆</a>
+        <a
+          v-link="{ path: '/sign-in' }"
+          class="sign_link"
+        >
+          登陆
+        </a>
       </li>
       <li class="nav_item">
-        <a v-link="{ path: '/sign-up' }">注册</a>
+        <a
+          v-link="{ path: '/sign-up' }"
+          class="sign_link"
+        >
+          注册
+        </a>
       </li>
     </ul>
   </div>
