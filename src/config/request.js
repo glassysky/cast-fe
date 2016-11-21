e41be9b718d6c2b4e83729b0549da5a47872e608
glassysky <873661157@qq.com>
2016-11-21 20:14:07 +0800

    into react

--- src/config/request.js
+++
@@ -1,9 +0,0 @@
-import LS from '../utils/localStorage';
-
-const PREFIX = '//localhost:3000';
-const TOKEN = LS.get('cast_jwt');
-
-export {
-  PREFIX,
-  TOKEN,
-};
--- src/utils/localStorage.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  get: (name) => localStorage.getItem(name),
-  set: (name, value) => {
-    localStorage.setItem(name, value);
-  },
-  remove: (name) => {
-    localStorage.removeItem(name);
-  },
-};
