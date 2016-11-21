af299c2cae6653b22f57587bb33071bcc29904c1
glassysky <873661157@qq.com>
2016-11-21 21:09:47 +0800

    react router init

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/Home/Index';
 import './index.css';
+import { Router, Route, hashHistory} from 'react-router';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+),document.getElementById('root'));
