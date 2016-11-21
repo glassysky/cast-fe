854754e3729c46406127f9ffa674776740d069d3
glassysky <873661157@qq.com>
2016-11-21 23:46:11 +0800

    react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/Home/Index';
-import './index.css';
-import { Router, Route, hashHistory} from 'react-router';
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+
+import reducers from './reducers/index.js'
+
+import './index.css'
+import App from './pages/Home/index'
+import SignIn from './pages/SignIn/index'
+import SignUp from './pages/SignUp/index'
+
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    routing: routerReducer
+  })
+)
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render((
-  <Router history={hashHistory}>
-    <Route path="/" component={App}/>
-  </Router>
-),document.getElementById('root'));
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App} />
+      <Route path="/signup" component={SignUp} />
+      <Route path="/signin" component={SignIn} />
+    </Router>
+  </Provider>
+),document.getElementById('root'))
--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './Index.css';
+import './index.css';
 
 class App extends Component {
   render() {
