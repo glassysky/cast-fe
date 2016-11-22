2ab04b8ca65a37f7bb95b6fa2383bfaf6e11f2f1
glassysky <873661157@qq.com>
2016-11-22 15:59:43 +0800

    nav component

--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './index.css';
 
+import Nav from '../../components/nav/index'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav></Nav>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to </h2>
